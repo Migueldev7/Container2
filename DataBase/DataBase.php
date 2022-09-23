a2804eb1a7b7d2d2a1e0d0374cc78aa27277801f
Miguel Oliveira <aprendizvulgo18@gmail.com>
2022-09-23 15:03:24 -0300

    Up

---
+++ DataBase/DataBase.php
@@ -0,0 +1,30 @@
+<?php
+
+class DataBase{
+
+private function connect(){
+    $this->conn = new mysqli("localhost" , "root" , "" , "container");
+}
+
+private function close(){
+    $this->conn->close();
+}
+
+public function execute($sql){
+    $this->connect();
+    $resultObject = $this->conn->query($sql);
+   
+    if(!($resultObject instanceof mysqli_result)) return;
+
+    $resultArr = [];
+    
+    while($r = mysqli_fetch_assoc($resultObject)){
+        $resultArr[] = $r;
+    }
+    $this->close();
+    return $resultArr;
+
+    ///ijiasbfuaobfaaa
+}
+
+}
