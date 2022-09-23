6ca5856680b44c3ecb9ee290151e0e062bcf6ad6
Pedro Teloeken <87155693+PedroTeloeken@users.noreply.github.com>
2022-09-23 15:49:42 -0300

    .

--- Service/Service.php
+++ Service/Service.php
@@ -1,1 +1,19 @@
 <?php
+require("DataBase/DataBase.php");
+require("Model/Model.php");
+
+
+class Service
+{
+
+    private $db;
+
+    public function __construct()
+    {
+        $this->db = new DataBase();
+    }
+
+    public function registerContainer($id , $name , $number , $status , $category){
+        $this->db->execute("INSERT INTO cliente (id, name, number, status, category) VALUES ($id , $name, $number , $status , $category");
+    }
+}
