5c1d685807b5a7202b7041fd36c9342055d86abb
Pedro Teloeken <87155693+PedroTeloeken@users.noreply.github.com>
2022-09-26 14:05:14 -0300

    .

--- Service/Service.php
+++ Service/Service.php
@@ -14,6 +14,19 @@ class Service
     }
 
     public function registerContainer($id , $name , $number , $status , $category){
-        $this->db->execute("INSERT INTO cliente (id, name, number, status, category) VALUES ($id , $name, $number , $status , $category");
+        $this->db->execute("INSERT INTO cliente(id, name, number, status, category) VALUES ($id , $name, $number , $status , $category");
     }
+
+    public function Delete($id){
+        $this->db->execute("DELETE FROM cliente WHERE id = $id");
+    }
+ 
+    public function Updade($id , $name , $number , $status , $category){
+        $this->db->execute("UPDATE cliente SET name = $name ,number= $number , status = $status , category = $category WHERE id = $id");
+    }
+
+    public function SelectByID($id){
+        $this->db->execute("SELECT * FROM cliente WHERE id = $id");
+    }
+
 }
